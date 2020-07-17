c4725c671183e7619d9624ae65bd09c3955b1636
Carlos Lopez <krlosaren@gmail.com>
2020-07-16 22:44:46 -0400

    fixed modules

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-<<<<<<< HEAD
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
@@ -16,30 +15,10 @@ module.exports = {
     port: 4000,
     hot: true,
     open:true,
-=======
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-
-module.exports = {
-  entry: {
-    app : path.resolve(__dirname, './src/app.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: './src/[name].js',
-  },
-  mode: 'development',
-  devServer: {
-    port: 3500,
-    hot: true,
-    open: true,
->>>>>>> master
   },
   module: {
     rules: [
       {
-<<<<<<< HEAD
         test: /.js$/,
         use: 'babel-loader',
         exclude: /node_modules/,
@@ -51,38 +30,6 @@ module.exports = {
           'css-loader'
         ],
       },
-=======
-        test: /\.js$/,
-        use:'babel-loader',
-        exclude: /node_modules/,
-      },
-      // {
-      //   test: /\.html$/i,
-      //   use: [
-      //     'file-loader?name=[name].[ext]',
-      //     'extract-loader',
-      //     'html-loader'
-      //     ],
-      // },
-      // {
-      //   test: /\.pug$/,
-      //   use : [
-      //     {
-      //       loader: 'pug-loader',
-      //       options: {
-      //         pretty: true,
-      //       }
-      //     }
-      //   ]
-      // },
-      // {
-      //   test: /\.css$/,
-      //   use : [
-      //     'style-loader',
-      //     'css-loader',
-      //   ]
-      // },
->>>>>>> master
       {
         test: /\.s[ac]ss$/i,
         use: [
@@ -98,7 +45,6 @@ module.exports = {
       },
       {
         test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
-<<<<<<< HEAD
         use: {
           loader: 'file-loader',
           options: {
@@ -116,32 +62,4 @@ module.exports = {
       template: path.resolve(__dirname, './index.html')
     })
   ]
-=======
-        loader: 'file-loader',
-        options: {
-          outputPath: './src/assets/',
-          name: '[name].[ext]',
-        },
-      },
-    ]
-  },
-  plugins: [
-    // new MiniCssExtractPlugin({
-    //   filename: './src/styles/css/[name].css',
-    // }),
-    new HtmlWebpackPlugin({
-      alwaysWriteToDisk: true,
-      title: '[name].html',
-      template: path.resolve(__dirname, 'index.html'),
-      // filename: '[name].html'
-    }),
-  ],
-  // optimization: {
-  //   splitChunks: {
-  //     chunks: 'all',
-  //     minSize: 0,
-  //     name: 'commons',
-  //   }
-  // }
->>>>>>> master
 }
