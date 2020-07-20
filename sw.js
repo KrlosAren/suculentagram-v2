85ec15d2d77727b9183a44a012ad21c854f0fba3
Carlos Lopez <krlosaren@gmail.com>
2020-07-20 08:53:19 -0400

    fixed.

--- sw.js
+++ sw.js
@@ -1,57 +1,50 @@
+const VERSION = "v1";
 
-const APP_PREFIX = 'Suculentagram'
-const VERSION = 'V1'
-const CACHE_NAME = APP_PREFIX + VERSION
-const URLS = [
-  'Suculentagram/src/assets/images/2.jpg',
-  'Suculentagram/src/assets/images/5.jpg',
-  'Suculentagram/src/assets/images/7.jpg',
-  'Suculentagram/src/assets/images/partners-1.svg',
-  'Suculentagram/src/assets/images/partners-2.svg',
-  'Suculentagram/src/assets/images/partners-3.svg',
-  'Suculentagram/src/assets/images/partners-4.svg',
-  'Suculentagram/src/assets/images/partners-5.svg',
-  'Suculentagram/src/assets/images/partners-6.svg',
-  'Suculentagram/src/assets/images/Logo2.png',
-  'Suculentagram/src/app.js',
-  'Suculentagram/src/utils/index.js',
-  'Suculentagram/src/styles/scss/main.scss'
-]
+self.addEventListener("install", (event) => {
+  event.waitUntil(precache());
+});
 
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+  // get
+  if (request.method !== "GET") {
+    return;
+  }
 
+  // buscar en cache
+  event.respondWith(cachedResponse(request));
 
-// Respond with cached resources
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(request => {
-      if (request) {
-        return request || fetch(event.request)
-      }
-    }))
-})
+  // actualizar el cache
+  event.waitUntil(updateCache(request));
+});
 
-// Cache resources
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS)
-    })
-  )
-})
+async function precache() {
+  const cache = await caches.open(VERSION);
+  return cache.addAll([
+    "./src/assets/images/2.jpg",
+    "./src/assets/images/5.jpg",
+    "./src/assets/images/7.jpg",
+    "./src/assets/images/partners-1.svg",
+    "./src/assets/images/partners-2.svg",
+    "./src/assets/images/partners-3.svg",
+    "./src/assets/images/partners-4.svg",
+    "./src/assets/images/partners-5.svg",
+    "./src/assets/images/partners-6.svg",
+    "./src/assets/images/Logo2.png",
+    "./src/app.js",
+    "./src/utils/index.js",
+    "./src/styles/scss/main.scss",
+  ]);
+}
 
-// Delete outdated caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(function (keyList) {
-      var cacheWhitelist = keyList.filter(key => {
-        return key.indexOf(APP_PREFIX)
-      })
-      cacheWhitelist.push(CACHE_NAME)
+async function cachedResponse(request) {
+  const cache = await caches.open(VERSION);
+  const response = await cache.match(request);
+  return response || fetch(request);
+}
 
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          return caches.delete(keyList[i])
-        }
-      }))
-    }))
-})
+async function updateCache(request) {
+  const cache = await caches.open(VERSION);
+  const response = await fetch(request);
+  return cache.put(request, response);
+}
